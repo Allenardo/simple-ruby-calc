3596f98b5ed32883348c6477f4c6eef68fe3134a
Juan <juan@baseball.(none)>
2013-10-13 21:50:01 -0600

    Added clear total. Refactored Devan's code.

--- main.rb
+++ main.rb
@@ -1,74 +1,93 @@
-puts "|--- Simple calculator on ruby                             ---|"
-puts "|--- By: Juan Lizarazo                                     ---|"
-puts "|--- Contributor: Devan Allen                              ---|"
-
-puts "Welcome to our basic Calculator"
-
-def choices 
-    puts "Your current total is: #{$total}" 
-    puts "Enter '1' to add to our total"
-    puts "Enter '2' to subtract from our total"
-    puts "Enter '3' to multiply our total by a number"
-    puts "Enter '4' to divide our total by a number"
-    puts "Enter '5' to exit the Calculator"
-    $choice = gets.chomp.to_i
-    if $choice == 5
-            puts "Your final total is: #{$total}"
-	        puts "Thanks for using our Calculator!"
-	    end
+def print_header
+  puts "\e[H\e[2J"
+print <<header
+|--------------------------------------------------------------|
+|--- Simple calculator on ruby                              ---|
+|--- By: Devan Allen and Juan Lizarazo                      ---|
+|--------------------------------------------------------------|
+|  Welcome to our basic Calculator                             |
+header
 end
 
-def calculator 
-    $total = 0
-    choices
-    while $choice != 5
-        case $choice
-        when 1
-            puts "What number would you like to add to your total?"
-            number = gets.chomp.to_f
-            if number == 0
-                puts "You have to enter a number you nut!"
-                $total = $total 
-            else
-                $total += number
-            end
-            choices
-        when 2
-            puts "What number would you like to subtract from your total?"
-            number = gets.chomp.to_f
-            if number == 0
-                puts "Seriously? A number!"
-                $total = $total 
-            else
-                $total -= number
-            end
-            choices
-        when 3
-            puts "What number would you like to multiply your total by?"
-            number = gets.chomp.to_f
-            if number == 0
-                puts "You do know what a number is right?!?"
-                $total = $total 
-            else
-                $total *= number
-            end
-            choices
-        when 4
-            puts "What number would you like to divide your total by??"
-            number = gets.chomp.to_f
-            if number == 0
-                puts "A NUMBER come on!"
-                $total = $total 
-            else
-                $total /= number
-            end
-            choices
-        else
-            puts "Error you did not type in one of the numbers!" 
-            puts "Try again!"
-            choices
-        end
+def print_choices 
+  print <<options
+|--------------------------------------------------------------|
+|   \t1: Add to our total                                    |
+|   \t2: Subtract from our total                             |
+|   \t3: Multiply our total by a number                      |
+|   \t4: Divide our total by a number                        |
+|   \t5: Clear total                                         |
+|   \t6: Exit                                                |
+|--------------------------------------------------------------|
+      Your current total is: #{$total}
+|--------------------------------------------------------------|
+options
+
+  print "Enter option: "
+  choice = gets.chomp.to_i
+  run_calculator(choice)
+  puts "Your final total is: #{$total} \n\nThanks for using our Calculator!" if choice == 5
+
+end
+
+def run_calculator(choice)
+
+  case choice
+    
+  when 1
+    puts "What number would you like to add to your total?"
+    number = gets.chomp.to_f
+    if number == 0
+        puts "You have to enter a number you nut!"
+    else
+        $total += number
     end
+    
+  when 2
+    puts "What number would you like to subtract from your total?"
+    number = gets.chomp.to_f
+    if number == 0
+        puts "Seriously? A number!"
+    else
+        $total -= number
+    end
+
+  when 3
+    puts "What number would you like to multiply your total by?"
+    number = gets.chomp.to_f
+    
+    if number == 0
+        puts "You do know what a number is right?!?"
+    else
+        $total *= number
+    end
+    
+  when 4
+    puts "What number would you like to divide your total by??"
+    number = gets.chomp.to_f
+    if number == 0
+      puts "A NUMBER come on!"
+    else
+      $total /= number
+    end
+
+  when 5
+    $total = 0
+  
+  when 6
+    puts "Thanks for using our Calculator!\n\n\n"
+    $loop = false
+  
+  else
+    puts "Error you did not type in one of the numbers! \nTry again!";  sleep(2)
+  end
 end
 
-calculator
+
+$total = 0
+$loop = true
+
+while $loop
+  print_header
+  print_choices
+end
